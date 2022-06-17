653de026bae5a4d19286d519b5bc071cb90bd418
kia1016 <kia1016@gmail.com>
2022-06-17 16:43:36 -0400

    JavaScript_API

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let input = document.getElementById("search");
+let image = document.getElementById("gif");
+const btn = document.getElementById("submitSearch");
+let apiKey = prompt("Please enter your api key");
+
+btn.addEventListener("click", function () {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${input.value}`
+  )
+    .then((response) => response.json())
+    .then((result) => (image.src = result.data.images.original.url))
+    .catch((error) => console.log(error));
+});
